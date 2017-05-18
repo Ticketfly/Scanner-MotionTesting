95b00aa1ece7a67116ad5528ab945b18d5e3f64b
Kerry Washington <kerry.washington@ticketfly.com>
2017-05-17 22:09:03 -0700

    added outlets

--- EMSMotionTester/ViewController.swift
+++ EMSMotionTester/ViewController.swift
@@ -7,13 +7,68 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
+    //Motion
+    @IBOutlet weak var motionPitchValue: UILabel!
+    @IBOutlet weak var motionYawValue: UILabel!
+    @IBOutlet weak var motionRollValue: UILabel!
+    //Gyro
+    @IBOutlet weak var alpha: UILabel!
+    @IBOutlet weak var beta: UILabel!
+    @IBOutlet weak var gamma: UILabel!
+    //Device Acceleration
+    @IBOutlet weak var deviceAccelX: UILabel!
+    @IBOutlet weak var deviceAccelY: UILabel!
+    @IBOutlet weak var deviceAccelZ: UILabel!
+    //User Acceleration
+    @IBOutlet weak var userAccelX: UILabel!
+    @IBOutlet weak var userAccelY: UILabel!
+    @IBOutlet weak var userAccelZ: UILabel!
+    
+    @IBOutlet weak var timingIntervalLabel: UILabel!
+    
+    @IBOutlet weak var timingIntervalSlider: UISlider!
+    @IBOutlet weak var subtract: UIButton!
+    @IBOutlet weak var add: UIButton!
+    
+    @IBAction func startSensing(_ sender: Any) {
+        
+    }
+    
+    @IBAction func stopSensing(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addValue(_ sender: Any) {
+        
+    }
+    @IBAction func subtractValue(_ sender: Any) {
+        
+    }
+    
+    
+    var motionManager: CMMotionManager!
+    var timingInterval = 1.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        setupMotionManager()
+        
+        
     }
+    
+    func setupMotionManager() {
+    
+        timingIntervalSlider.value = Float(timingInterval)
+    
+    
+    }
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
